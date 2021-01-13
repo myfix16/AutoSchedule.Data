6fa2e40bb4afcda7933dbe391e928ef9b03e5e87
Xinyu Xie <119020059@link.cuhk.edu.cn>
2021-01-13 21:14:56 +0800

    update extract_field.py
    Co-authored-by: X M <myfix16@outlook.com>

--- extract_field.py
+++ extract_field.py
@@ -14,10 +14,11 @@ class FieldExtractor():
         self.doc = BeautifulSoup(open(source, encoding='utf-8'))
         self.subject_table: Tag = self.doc.select('#ACE_\$ICField102\$0')[0]
         self._names: List[str] = None
-        self._codes: List[str] = None
-        self._instructors: List[str] = None
-        self._times: List[str] = None
-        self._locations: List[str] = None
+        self._codes: List[List[str]] = None
+        self._instructors: List[List[str]] = None
+        self._times: List[List[str]] = None
+        self._locations: List[List[str]] = None
+        self._contents: list = None
 
     @property
     def names(self) -> List[str]:
@@ -30,7 +31,7 @@ class FieldExtractor():
         return self._names
 
     @property
-    def codes(self) -> List[str]:
+    def codes(self) -> List[List[str]]:
         """Get the code of every sessions.
         """
         if self._codes is None:
@@ -40,64 +41,91 @@ class FieldExtractor():
         return self._codes
 
     @property
-    def instructors(self) -> List[str]:
+    def instructors(self) -> List[List[str]]:
         if self._instructors is None:
             self._read_details()
         return self._instructors
 
     @property
-    def times(self) -> List[str]:
+    def times(self) -> List[List[str]]:
         if self._times is None:
             self._read_details()
         return self._times
 
     @property
-    def locations(self) -> List[str]:
+    def locations(self) -> List[List[str]]:
         if self._locations is None:
             self._read_details()
         return self._locations
+
+    @property
+    def contents(self) -> list:
+        """Get the content of every session, return all div's.
+        """
+        if self._contents is None:
+            self._contents: ResultSet = self.subject_table.select("a[id^='win0div$ICField106$']")
+        return self._contents
+
+
+
+
+
+
 
     # TODO: The function doesn't parse the html correctly. In specific, the name and details are not matched in some courses like ERG3020.
     def _read_details(self) -> None:
         self._instructors = []
         self._times = []
         self._locations = []
-        details_table: ResultSet = self.subject_table.findAll(
-            'table', id=re.compile('SSR_CLSRCH_MTG1\$scroll\$\d+'))
-        for detail in details_table:
-            rows = detail.find_all(
-                'tr', id=re.compile('trSSR_CLSRCH_MTG1\$\d+_row\d'))
-            if len(rows) == 1:
-                row: Tag = rows[0]
-                self._times.append(
-                    row.find('span', id=re.compile('MTG_DAYTIME\$\d')).string)
-                self._locations.append(
-                    row.find('span', id=re.compile('MTG_ROOM\$\d')).string)
-                self._instructors.append(
-                    row.find('span', id=re.compile('MTG_INSTR\$\d')).string)
-            elif len(rows) > 1:
-                self._locations.append(
-                    rows[0].find('span', id=re.compile('MTG_ROOM\$\d')).string)
-                self._instructors.append(
-                    rows[0].find('span', id=re.compile('MTG_INSTR\$\d')).string)
-                times: List[str] = [
-                    row.find('span', id=re.compile('MTG_DAYTIME\$\d')).string for row in rows]
-                self._times.append(";".join(times))
-            else:
-                raise ValueError(
-                    f"The number of detail rows is incorrect. Expect an integer larger than 1, actual value: {len(rows)}")
+        # Loop through all sessions and gather session information.
+        for div in self.contents:
+            instructors = []
+            times = []
+            locations = []
+            details_table: ResultSet = div.findAll('table', id=re.compile('SSR_CLSRCH_MTG1\$scroll\$\d+'))
+            # Deal with sessions that have multiple session time.
+            for detail in details_table:
+                rows = detail.find_all(
+                    'tr', id=re.compile('trSSR_CLSRCH_MTG1\$\d+_row\d'))
+                if len(rows) == 1:
+                    row: Tag = rows[0]
+                    times.append(
+                        row.find('span', id=re.compile('MTG_DAYTIME\$\d')).string)
+                    locations.append(
+                        row.find('span', id=re.compile('MTG_ROOM\$\d')).string)
+                    instructors.append(
+                        row.find('span', id=re.compile('MTG_INSTR\$\d')).string)
+                elif len(rows) > 1:
+                    locations.append(
+                        rows[0].find('span', id=re.compile('MTG_ROOM\$\d')).string)
+                    instructors.append(
+                        rows[0].find('span', id=re.compile('MTG_INSTR\$\d')).string)
+                    time: List[str] = [
+                        row.find('span', id=re.compile('MTG_DAYTIME\$\d')).string for row in rows]
+                    times.append(";".join(time))
+                else:
+                    raise ValueError(
+                        f"The number of detail rows is incorrect. Expect an integer larger than 1, actual value: {len(rows)}")
+            self._instructors.append(instructors)
+            self._times.append(times)
+            self._locations.append(locations)
 
     def write_to_csv(self, path: str) -> None:
         headers: List[str] = ["Name", "Code", "Time", "Instructor", "Location"]
-        contents: List[Tuple[str]] = zip(
-            self.names, self.codes, self.times, self.instructors, self.locations)
+        # contents: List[Tuple[str]] = zip(
+        #     self.names, self.codes, self.times, self.instructors, self.locations)
+        
+        contents: List[List[str]]=[]
+        for i in range(len(self._names)):
+            for j in range(len(self._codes[i])):
+                contents.append([self._names[i], self.codes[i][j], self.times[i][j], self.instructors[i][j], self.locations[i]])
         with open(path, 'w+', encoding='utf-8', newline='') as f:
             f_csv = csv.writer(f)
             f_csv.writerow(headers)
             f_csv.writerows(contents)
 
 
-subjects: List[str] = [name[:-5] for name in os.listdir(r"C:\Users\MyFix\Documents\Projects\AutoSchedule.Data\pages")]
+subjects: List[str] [name[:-5] for name in os.listdir(r"C:\Users\MyFix\Documents\Projects\AutoSchedule.Data\pages")]
 subject = ["Accounting"]
 for subject in subjects:
     fe = FieldExtractor(subject)
